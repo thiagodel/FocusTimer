d6c66570c158bc26ca0f416864dd021682de69cc
Ruan-2704 <ruan.cidadania@gmail.com>
2025-10-29 23:00:36 -0300

    Estrutura inicial do backend

---
+++ backend-java/src/main/java/com/focustimer/focustimer/FocustimerApplication.java
@@ -0,0 +1,13 @@
+package com.focustimer.focustimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocustimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocustimerApplication.class, args);
+	}
+
+}
